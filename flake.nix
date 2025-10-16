9088f0ced7fb9428ff66f30ef519eab44c0dbb25
lordofwizard <advaitpandharpurkar619@gmail.com>
2025-10-17 01:04:59 +0530

    New changes

--- flake.nix
+++ flake.nix
@@ -1,29 +1,29 @@
 {
-  description = "Minimal NixOS configuration";
+  description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      nixus = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./hardware-configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.lordofwizard = import ./home.nix;
-          }
-        ];
-      };
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        
+        # Import the home-manager NixOS module
+        home-manager.nixosModules.home-manager
+        
+        # Configure home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.lordofwizard = import ./home.nix;
+        }
+      ];
     };
   };
 }
--- home.nix
+++ home.nix
@@ -4,18 +4,76 @@
   home.username = "lordofwizard";
   home.homeDirectory = "/home/lordofwizard";
 
+
+  # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
+    nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+
+    btop  # replacement of htop/nmon
+
+
+    # system tools
+    lm_sensors # for `sensors` command
+    pciutils # lspci
+    usbutils # lsusb
   ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "lordofwizard";
+    userEmail = "advaitpandharpurkar619@gmail.com";
+  };
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
 
   programs.bash = {
     enable = true;
     enableCompletion = true;
-  };
 
-  programs.git = {
-    enable = true;
-    userName = "lordofwizard";
-    userEmail = "lordofwizard@example.com";
   };
 
   home.stateVersion = "25.05";
